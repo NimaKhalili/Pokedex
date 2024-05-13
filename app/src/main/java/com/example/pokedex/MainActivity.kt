9f4da3fc93871a5de050ad8c607a0931e67d1f3c
NimaKhalili <khalili7091@gmail.com>
2024-05-13 14:41:58 +0430

    Add MainActivity.kt NavComponents for two page

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -3,35 +3,119 @@ package com.example.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.pokedex.ui.theme.PokedexTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootGraph
+import com.ramcosta.composedestinations.generated.NavGraphs
+import com.ramcosta.composedestinations.generated.destinations.PostScreenDestination
+import com.ramcosta.composedestinations.generated.destinations.ProfileScreenDestination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
-
+                DestinationsNavHost(navGraph = NavGraphs.root)
             }
         }
     }
 }
 
+@Destination<RootGraph>
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PokemonListScreen() {
+
 }
 
-@Preview(showBackground = true)
+@Destination<RootGraph>
 @Composable
-fun GreetingPreview() {
-    PokedexTheme {
-        Greeting("Android")
+fun PokemonDetailScreen(
+    color: Int,
+    name: String
+) {
+    val dominantColor = remember {
+        Color(color)
+    }
+
+    val pokemonName = remember {
+        name
+    }
+
+}
+
+@Destination<RootGraph>(start = true)
+@Composable
+fun LoginScreen(
+    navigator: DestinationsNavigator
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login Screen")
+        Button(onClick = {
+            navigator.navigate(
+                ProfileScreenDestination(
+                )
+            )
+        }) {
+            Text("Go to Profile Screen")
+        }
+    }
+}
+
+@Destination<RootGraph>
+@Composable
+fun ProfileScreen(
+    navigator: DestinationsNavigator,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            navigator.navigate(PostScreenDestination())
+        }) {
+            Text("Go to Post Screen")
+        }
+    }
+}
+
+@Destination<RootGraph>
+@Composable
+fun PostScreen(
+    showOnlyPostsByUser: Boolean = false
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Post Screen, $showOnlyPostsByUser")
     }
 }
