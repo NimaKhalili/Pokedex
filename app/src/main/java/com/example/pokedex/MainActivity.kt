2e3a49710951e2111811a9a7e7a80215db2453fd
NimaKhalili <khalili7091@gmail.com>
2024-05-13 12:28:45 +0430

    Remove MainActivity.kt useless commands

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -8,13 +8,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pokedex.ui.theme.PokedexTheme
-//Test branch
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
-                //test branch 3
+
             }
         }
     }
