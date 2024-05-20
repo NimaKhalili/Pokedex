95667274f3652f2cb842c411f8c361b19ab9893c
NimaKhalili <khalili7091@gmail.com>
2024-05-20 15:31:45 +0430

    Remove MainActivity.kt unused codes

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -3,37 +3,15 @@ package com.example.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.pokedex.ui.theme.PokedexTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.generated.NavGraphs
-import com.ramcosta.composedestinations.generated.destinations.PostScreenDestination
-import com.ramcosta.composedestinations.generated.destinations.ProfileScreenDestination
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import dagger.hilt.android.AndroidEntryPoint
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -68,56 +46,3 @@ fun PokemonDetailScreen(
     }
 
 }
-
-//@Destination<RootGraph>(start = true)
-//@Composable
-//fun LoginScreen(
-//    navigator: DestinationsNavigator
-//) {
-//    Column(
-//        modifier = Modifier.fillMaxSize(),
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ) {
-//        Text("Login Screen")
-//        Button(onClick = {
-//            navigator.navigate(
-//                ProfileScreenDestination(
-//                )
-//            )
-//        }) {
-//            Text("Go to Profile Screen")
-//        }
-//    }
-//}
-//
-//@Destination<RootGraph>
-//@Composable
-//fun ProfileScreen(
-//    navigator: DestinationsNavigator,
-//) {
-//    Column(
-//        modifier = Modifier.fillMaxSize(),
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ) {
-//        Button(onClick = {
-//            navigator.navigate(PostScreenDestination())
-//        }) {
-//            Text("Go to Post Screen")
-//        }
-//    }
-//}
-//
-//@Destination<RootGraph>
-//@Composable
-//fun PostScreen(
-//    showOnlyPostsByUser: Boolean = false
-//) {
-//    Box(
-//        modifier = Modifier.fillMaxSize(),
-//        contentAlignment = Alignment.Center
-//    ) {
-//        Text(text = "Post Screen, $showOnlyPostsByUser")
-//    }
-//}
